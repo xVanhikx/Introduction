81f4aa90d8fd000f9a6584041be46602634f7e59
Antanaks <OUT-Alekseev.M.S@sbertech.ru>
2025-02-13 09:02:36 +0300

    add task

---
+++ src/main/java/ru/ifellow/introdution/FirstStep.java
@@ -0,0 +1,89 @@
+package ru.ifellow.introdution;
+
+public class FirstStep {
+
+    public int sum (int x, int y){
+
+    }
+
+    public int mul (int x, int y){
+
+    }
+
+    public int div (int x, int y){
+
+    }
+
+    public int mod (int x, int y){
+
+    }
+
+    public boolean isEqual (int x, int y){
+
+    }
+
+    public boolean isGreater (int x, int y){
+
+    }
+
+    public boolean isInsideRect(int xLeft, int yTop, int xRight, int yBottom, int x, int y){
+
+    }
+
+    public int sum(int[] array){
+
+    }
+
+    public int mul(int[] array){
+
+    }
+
+    public int min(int[] array){
+
+    }
+
+    public int max(int[] array){
+
+    }
+
+    public double average(int[] array){
+
+    }
+
+    public boolean isSortedDescendant(int[] array){
+
+    }
+
+    public void cube(int[]array){
+
+    }
+
+    public boolean find(int[]array, int value){
+
+    }
+
+    public void reverse(int[]array){
+
+    }
+
+    public boolean isPalindrome(int[]array){
+
+    }
+
+    public int sum(int[][] matrix){
+
+    }
+
+    public int max(int[][] matrix){
+
+    }
+
+    public int diagonalMax(int[][] matrix){
+
+    }
+
+    public boolean isSortedDescendant(int[][] matrix){
+
+    }
+
+}
